07aaf7c27caa0257f4d7c21e08f20d9a001823f0
Lavaver <yansuan_game@qq.com>
2024-05-18 15:04:08 +0800

    Add KMCSharp.PowerSharedProject Folder

---
+++ KMCSharp.PowerSharedProject/Auth/WarpedAuhenticator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace KMCSharp.Auth
+{
+    public class WarpedAuhenticator : IAuthenticator
+    {
+        private readonly AuthenticationInfo _info;
+
+        /// <summary>
+        ///     创建反常验证器
+        /// </summary>
+        /// <param name="info">要包装的验证信息</param>
+        public WarpedAuhenticator(AuthenticationInfo info)
+        {
+            _info = info;
+        }
+
+        /// <summary>
+        ///     标注包装验证器
+        /// </summary>
+        public string Type
+        {
+            get { return "LibWarped"; }
+        }
+
+        public AuthenticationInfo Do()
+        {
+            return _info;
+        }
+
+        public Task<AuthenticationInfo> DoAsync(CancellationToken token)
+        {
+            return Task.Factory.StartNew(() => _info, token);
+        }
+    }
+}
---
+++ KMCSharp.PowerSharedProject/Launcher/LaunchHandleExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KMCSharp.Launcher
+{
+    public static class LaunchHandleExtensions
+    {
+        public static bool SetTitle(this LaunchHandle handle, string title)
+        {
+            try
+            {
+                SetWindowText(handle.Process.MainWindowHandle, title);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static string GetTile(this LaunchHandle handle)
+        {
+            try
+            {
+                return handle.Process.MainWindowTitle;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static void Kill(this LaunchHandle handle)
+        {
+            handle.Process.Kill();
+        }
+
+
+        [DllImport("User32.dll")]
+        public static extern int SetWindowText(IntPtr winHandle, string title);
+    }
+}
